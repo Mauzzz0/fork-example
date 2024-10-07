aff1d7c318f2661dcde9af89339924a7801590e4
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-10-07 10:31:46 +0300

    feat: Добавлен неработающий проект

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+// Chalk test
+console.log(chalk.green('Chalk works!'));
+
+
+// TypeScript test
+type Pair2<T> = [first: T, T];
+const pair: Pair2<number> = [1, 2];
+
+console.log('TypeScript works!');
